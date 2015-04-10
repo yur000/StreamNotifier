e86c842dbbe8b82c835b1a7b1c642cf19dabd0ef
Yuri Babinets <admin@cosmostv.tk>
2015-04-10 21:39:20 +0300

    Added class for transp. of settings

---
+++ settingsstruct.h
@@ -0,0 +1,14 @@
+#ifndef SETTINGSSTRUCT
+#define SETTINGSSTRUCT
+
+#include <QString>
+class settingsContainer
+{
+public:
+    int timeToCheck;
+    bool isSound;
+    QString soundPath;
+};
+
+#endif // SETTINGSSTRUCT
+
