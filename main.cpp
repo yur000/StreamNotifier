ff31b132a868672f9afccb78fc3746e022f5821b
Yuri Babinets <admin@cosmostv.tk>
2015-04-10 21:37:17 +0300

    Renaming val.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    core app;
-    return a.exec();
+    QApplication app(argc, argv);
+    core mainApp;
+    return app.exec();
 }
